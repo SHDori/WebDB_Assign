ab0b6557164ddc735a385f6befff8285dc50d9b8
SeungHwan <vw9801@naver.com>
2021-11-06 22:23:14 +0900

    first commit

---
+++ src/main/java/com/webdb/UsedTradeBoard/UsedTradeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.webdb.UsedTradeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTradeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedTradeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webdb/UsedTradeBoard/UsedTradeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdb.UsedTradeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedTradeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
